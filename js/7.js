2774365451f9ffc5ee6dd109f88054064088fd17
Lisandro Civili <lisandrocivili13@gmail.com>
2023-11-26 23:41:59 -0300

    ejercicio 7 finalizado

--- js/7.js
+++ js/7.js
@@ -1,34 +1,143 @@
-class Contacts {
-  #name;
-  #phoneNumber;
-  constructor(nameParam, phoneNUmberParam) {
-    this.#name = nameParam;
-    this.#phoneNumber = phoneNUmberParam;
+class Contactos {
+  #nombre;
+  #numero;
+  constructor(nombreParam, numeroParam) {
+    this.#nombre = nombreParam;
+    this.#numero = numeroParam;
   }
 
-  get name() {
-    return this.#name;
+  get nombre() {
+    return this.#nombre;
   }
-  set name(nameString) {
-    if (typeof nameString === "string" && nameString !== null) {
-      this.#name = nameString;
+  set nombre(nombreString) {
+    if (typeof nombreString === "string" && nombreString !== null) {
+      this.#nombre = nombreString;
     } else {
       alert("Ingrese un valor correcto");
     }
   }
 
-  get phoneNumber() {
-    return this.#phoneNumber;
+  get numero() {
+    return this.#numero;
   }
-  set phoneNumber(phoneNumberInt) {
-    if (typeof phoneNumberInt === "number" && !isNaN(phoneNumberInt) && phoneNumberInt !== null) {
-      this.#phoneNumber = phoneNumberInt;
+  set numero(numeroInt) {
+    if (typeof numeroInt === "number" && !isNaN(numeroInt) && numeroInt !== null && numeroInt.length === 10) {
+      this.#numero = numeroInt;
     } else {
       alert("Ingrese un valor correcto");
     }
   }
 
-  añadirContacto(name, number){
-    
-  }
+  
 }
+class Agenda{
+  constructor(){
+    this.capacidad = 10;
+    this.listaContactos = [];
+  }
+
+  aniadirContacto(contactoNuevo){
+    
+    const chequeo = this.listaContactos.find(contactoExiste => contactoExiste.nombre === contactoNuevo.nombre);
+
+    if (!chequeo) {
+      if (this.listaContactos.length < this.capacidad) {
+        
+        this.listaContactos.push(contactoNuevo);
+      }else{
+        alert("Agenda llena, elimine algunos contactos para poder guardar nuevos.");
+      }
+    }else{
+      alert("Este contacto ya existe");
+    }
+
+  }
+
+  existeContacto(contactoNuevo){
+    const chequeo = this.listaContactos.find(contactoExiste => contactoExiste.nombre === contactoNuevo);
+    if (chequeo) {
+      alert("El contacto se encuentra en la agenda.")
+    }else{
+      alert("El contacto no se encuentra en la agenda.")
+    }
+  }
+
+  mostrarContactos(){
+    for (let i = 0; i < this.listaContactos.length; i++) {
+      document.write(`Nombre: ${this.listaContactos[i].nombre}<br>Celular: ${this.listaContactos[i].numero}<br><br>`)      
+    }
+  }
+
+  buscarContacto(contactoBuscar){
+    const chequeo = this.listaContactos.find(contactoBuscado => contactoBuscado.nombre === contactoBuscar);
+    const indice = this.listaContactos.indexOf(chequeo);
+    if (chequeo) {
+      alert(`Nombre: ${this.listaContactos[indice].nombre}\nNumero: ${this.listaContactos[indice].numero}`);
+    }else{
+      alert("El contacto no se encuentra en la agenda.")
+    }
+  }
+
+  eliminarContacto(contactoEliminar){
+    const chequeo = this.listaContactos.find(contactoBuscado => contactoBuscado.nombre === contactoEliminar);
+    const indice = this.listaContactos.indexOf(chequeo);
+    if (chequeo) {
+      this.listaContactos.splice(indice, 1);
+      alert("Contacto borrado exitosamente")
+    }else{
+      alert("El contacto no se encuentra en la agenda.")
+    }
+  }
+
+  agendaLlena(){
+    if (this.listaContactos.length >= this.capacidad) {
+      alert("Agenda llena.")
+    }
+  }
+
+  huecosLibres(){
+    if (this.listaContactos.length < this.capacidad) {
+      alert(`Espacios restantes: ${this.capacidad - this.listaContactos.length}`)
+    }else{
+      alert("Agenda llena.");
+    }
+  }
+
+}
+
+const contacto1 = new Contactos("Lisandro Civili", 3816174315);
+const contacto2 = new Contactos("Martina Gonzalez", 3813456680);
+const contacto3 = new Contactos("Geronimo Gutierrez", 3813152847);
+const contacto4 = new Contactos("Victoria Camacho", 3813680539);
+
+const agenda = new Agenda();
+
+agenda.aniadirContacto(contacto1);
+agenda.aniadirContacto(contacto2);
+agenda.aniadirContacto(contacto3);
+agenda.aniadirContacto(contacto4);
+agenda.agendaLlena();
+
+let opcion;
+do {
+  opcion = parseInt(prompt("Menu\n1-Buscar contacto\n2-Verificar contacto\n3-Eliminar contacto\n4-Espacio libre"))
+  
+  switch (opcion) {
+    case 1:
+      agenda.buscarContacto(prompt("Buscar contacto:"));
+      break;
+    case 2:
+      agenda.existeContacto(prompt("Verificar contacto existente:"));
+      break;
+    case 3:
+      agenda.eliminarContacto(prompt("Eliminar contacto:"));
+      break;
+    case 4:
+      agenda.huecosLibres();
+      break;
+  
+    default:
+      break;
+  }
+} while (opcion !== 0);
+
